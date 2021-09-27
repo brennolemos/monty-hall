db9bdd0399b635ca17f8e8384f265ceedf63e11b
Breno Lemos <brenomiros@gmail.com>
2021-09-27 20:03:01 -0300

    Adding events to the door

--- components/Door.tsx
+++ components/Door.tsx
@@ -2,20 +2,33 @@ import DoorModel from "../model/door";
 import styles from "../styles/Door.module.css";
 
 interface DoorProps {
-  door: DoorModel;
+  value: DoorModel;
+  onChange: (newDoor: DoorModel) => void;
 }
 
-export default function Door({ door }: DoorProps) {
-  const selected = door.selected ? styles.selected : "";
+export default function Door(props: DoorProps) {
+  const door = props.value;
+  const selected = door.selected && !door.opened ? styles.selected : "";
+
+  const toggleSelection = (e) => props.onChange(door.toggleSelection());
+  const open = (e) => {
+    e.stopPropagation();
+    props.onChange(door.open());
+  };
+
+  const renderDoor = () => (
+    <div className={styles.door}>
+      <div className={styles.number}>{door.number}</div>
+      <div className={styles.handle} onClick={open}></div>
+    </div>
+  );
 
   return (
-    <div className={styles.area}>
+    <div className={styles.area} onClick={toggleSelection}>
       <div className={`${styles.frame} ${selected}`}>
-        <div className={styles.door}>
-          <div className={styles.number}>{door.number}</div>
-          <div className={styles.handle}></div>
-        </div>
+        {door.opened ? false : renderDoor()}
       </div>
+
       <div className={styles.ground}></div>
     </div>
   );
