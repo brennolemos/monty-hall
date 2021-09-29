790dc09e5f9a4135b0d7c6e766554053d6db83bf
Breno Lemos <brenomiros@gmail.com>
2021-09-29 11:46:13 -0300

    Using router

--- pages/game/[doors]/[awarded].tsx
+++ pages/game/[doors]/[awarded].tsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
-import Door from "../tmp/Door";
-import { createDoors, refreshDoors } from "../functions/door";
+import { useEffect, useState } from "react";
+import Door from "../../../tmp/Door";
+import { createDoors, refreshDoors } from "../../../functions/door";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-import styles from "../styles/Game.module.css";
+import styles from "../../../styles/Game.module.css";
 
 export default function Game() {
-  const [doors, setDoors] = useState(createDoors(3, 2));
+  const router = useRouter();
+  const [doors, setDoors] = useState([]);
+
+  useEffect(() => {
+    const doors = +router.query.doors;
+    const awarded = +router.query.awarded;
+    setDoors(createDoors(doors, awarded));
+  }, [router?.query]);
 
   function renderDoors() {
     return doors.map((door) => {
