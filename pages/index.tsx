45de0e2597d3e2c3139e2e94eabb552284ae820f
Breno Lemos <brenomiros@gmail.com>
2021-09-29 13:59:38 -0300

    Finish form

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,37 @@ import Card from "../components/Card";
 import Link from "next/link";
 
 import styles from "../styles/Form.module.css";
+import NumberInput from "../components/NumberInput";
+import { useState } from "react";
 
 export default function Form() {
+  const [doorsQty, setDoorsQty] = useState(3);
+  const [awarded, setAwarded] = useState(1);
+
   return (
     <div className={styles.form}>
       <div>
         <Card bgcolor="#c0392c">
           <h1>Monty Hall</h1>
         </Card>
-        <Card />
+        <Card>
+          <NumberInput
+            value={doorsQty}
+            onChange={(newQty) => setDoorsQty(newQty)}
+            text="Doors Qty?"
+          />
+        </Card>
       </div>
       <div>
-        <Card />
+        <Card>
+          <NumberInput
+            value={awarded}
+            onChange={(newDoor) => setAwarded(newDoor)}
+            text="Awarded Door"
+          />
+        </Card>
         <Card bgcolor="#28a085">
-          <Link href={`/game/4/2`}>
+          <Link href={`/game/${doorsQty}/${awarded}`}>
             <h2 className={styles.link}>Start</h2>
           </Link>
         </Card>
